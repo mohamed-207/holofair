655e9501c5f6fdb5cc35f9a9060e93861fabbd38
mohammed alkilani <58834459+falconiq88@users.noreply.github.com>
2022-06-29 19:03:32 +0300

    main

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::middleware('throttle:60,1')->prefix('users')->group(function () {
 
-    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-        return $request->user();
-    });
+
     Route::post('signup','\App\Http\Controllers\AuthController@Signup');
     Route::post('signin','\App\Http\Controllers\AuthController@Signin');
     Route::get('holofair','\App\Http\Controllers\AuthController@show');
